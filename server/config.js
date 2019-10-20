581e285452e918e28bbc7a71233701979b2364ce
Mhidalgo <mhidalgo@inticousa.com>
2019-10-20 18:40:02 -0500

    config

--- server/config.js
+++ server/config.js
@@ -2,7 +2,9 @@ const config = {
   port: 8084,
   allowedOrigins: [
     'http://127.0.0.1:5501',
-    'http://localhost:5501'
+    'http://localhost:5501',
+    'https://amda-amazon.herokuapp.com',
+    'https://melyhc.github.io/amazonas-hackathon-amda'
   ],
   key_twilio: 'ACc8e338f32573ac5946f10cdd89eac7e6',
   pass_twilio: 'c035261ec13874994e1dc84d3f3b7f57',
