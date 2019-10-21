6e8ec1ea88ca252d8b844c18ef5b55f912219dde
Mhidalgo <mhidalgo@inticousa.com>
2019-10-20 20:49:59 -0500

    url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sendSms = document.getElementById('send-sms');
 
 sendSms.addEventListener('click', ()=>{
-  fetch('http://localhost:8084/send-sms', {
+  fetch('https://amda-amazon.herokuapp.com/send-sms', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
